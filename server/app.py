1e6af22c61919a64257c93b48f14b55dea4f7c94
jovanjoto <jovan_n@mail.com>
2024-09-04 02:14:39 +0800

    added port

--- server/app.py
+++ server/app.py
@@ -51,6 +51,7 @@ def send_email():
     return{"message" : "Failed to send email", "error" : str(e)}, 500
                  
 if __name__ == "__main__":
-  app.run(debug=True)
+  port = int(os.environ.get("PORT", 5000))
+  app.run(host='0.0.0.0', port=port, debug=True)
   
-  
+  2
